b6090727f8fc4ac99d0ab8a6ada532691e5fa9a7
ainhoa <ainhoamiret@gmail.com>
2025-12-17 19:46:42 +0100

    feat(Nibiru-Home/sprints#19): Api proxy images

---
+++ src/main/resources/db/migration/V3__new_products.sql
@@ -0,0 +1,13 @@
+
+
+INSERT INTO products (name, price, description, stock, style, image, category_id) VALUES ('Nuevo Producto', 45.50, 'Descripción del nuevo producto', 10, 'Moderno', '/images/new.jpg', 1);
+INSERT INTO products (name, price, description, stock, style, image, category_id) VALUES ('Nuevo Producto', 45.50, 'Descripción del nuevo producto', 10, 'Moderno', '/images/new.jpg', 1);
+INSERT INTO products (name, price, description, stock, style, image, category_id) VALUES ('Nuevo Producto', 45.50, 'Descripción del nuevo producto', 10, 'Moderno', '/images/new.jpg', 1);
+INSERT INTO products (name, price, description, stock, style, image, category_id) VALUES ('Nuevo Producto', 45.50, 'Descripción del nuevo producto', 10, 'Moderno', '/images/new.jpg', 1);
+INSERT INTO products (name, price, description, stock, style, image, category_id) VALUES ('Nuevo Producto', 45.50, 'Descripción del nuevo producto', 10, 'Moderno', '/images/new.jpg', 1);
+INSERT INTO products (name, price, description, stock, style, image, category_id) VALUES ('Nuevo Producto', 45.50, 'Descripción del nuevo producto', 10, 'Moderno', '/images/new.jpg', 1);
+INSERT INTO products (name, price, description, stock, style, image, category_id) VALUES ('Nuevo Producto', 45.50, 'Descripción del nuevo producto', 10, 'Moderno', '/images/new.jpg', 1);
+INSERT INTO products (name, price, description, stock, style, image, category_id) VALUES ('Nuevo Producto', 45.50, 'Descripción del nuevo producto', 10, 'Moderno', '/images/new.jpg', 1);
+INSERT INTO products (name, price, description, stock, style, image, category_id) VALUES ('Nuevo Producto', 45.50, 'Descripción del nuevo producto', 10, 'Moderno', '/images/new.jpg', 1);
+INSERT INTO products (name, price, description, stock, style, image, category_id) VALUES ('Nuevo Producto', 45.50, 'Descripción del nuevo producto', 10, 'Moderno', '/images/new.jpg', 1);
+
