246f182b764170694bcfbc9f4cc594cbc2ddacb2
ainhoa <ainhoamiret@gmail.com>
2025-12-18 11:42:41 +0100

    feat(Nibiru-Home/sprints#24): Editar base de datos

--- src/main/java/tienda_back/domain/service/impl/ProductServiceImpl.java
+++ src/main/java/tienda_back/domain/service/impl/ProductServiceImpl.java
@@ -8,7 +8,10 @@ import tienda_back.domain.repository.ProductRepository;
 import tienda_back.domain.service.ProductService;
 import org.springframework.stereotype.Service;
 
+import org.springframework.transaction.annotation.Transactional;
+
 @Service
+@Transactional
 public class ProductServiceImpl implements ProductService {
     private final ProductRepository productRepository;
 
